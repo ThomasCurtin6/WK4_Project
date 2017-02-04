b52898f7e3e4e3f5472f0c0f49a9d17ae5eeab39
Thomas Curtin <e41538@lahsic.com>
2017-02-04 10:34:00 -0600

    Saturday AM
    Initial update with data get and unzip, first code for getting
    activities and features written

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,15 @@
+
 #set the working directory for the new project
+prjdir<-"C:/Users/e41538/Desktop/GitHub/WK4_Project"
+setwd(prjdir)
 
-setwd("C:/Users/e41538/Desktop/GitHub/WK4_Project")
-dir()
-
-#Load libraries I like to work with
+#Load some libraries I like to work with
+library(downloader)
+library(data.table)
+library(reshape2)
 library(curl)
 library(dplyr)
 library(tidyr)
-library(downloader)
-library(data.table)
 
 # Create one R script called run_analysis.R that does the following.
 #
@@ -19,3 +20,20 @@ library(data.table)
 #    4.Appropriately labels the data set with descriptive variable names. 
 #    5.From the data set in step 4, creates a second, independent tidy data set 
 #      with the average of each variable for each activity and each subject.
+
+# 0.  Get the data
+source<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zippy <-"HARDATA"
+download(source,zippy,mode="wb")
+unzip(zippy)
+
+#1A Load the activities 
+mydir<-"UCI HAR Dataset"
+activityNames<-read.table("UCI HAR Dataset/activity_labels.txt")
+activityNames
+#1b load the features
+featureNames<-read.table("UCI HAR Dataset/features.txt")
+featureNames
+
+
+#1B  Load the training and test data sets
